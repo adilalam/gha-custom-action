d9ec3fdb551599928f6b06908a5d13ac47b41c6b
Md Adil Alam <mdadilalam@Mds-MacBook-Air.local>
2024-11-11 10:17:44 +0530

    add custome deploy action

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -1,9 +1,18 @@
 const core = require('@actions/core')
-const github = require('@actions/github')
+// const github = require('@actions/github')
 const exec = require('@actions/exec')
 
 function run() {
-    core.notice('Hello my custom JavaScript Action')
+    // 1 get input value
+    const bucket = core.getInput('bucket', { required: true });
+    const bucketRegion = core.getInput('bucket-region', { required: true });
+    const distFolder = core.getInput('dist-folder', { required: true });
+
+    // 2 upload files
+    const s3Uri = `s3://${bucket}`
+    exec.exec(`aws s3 sync  ${distFolder} ${s3Uri} -- region ${bucketRegion}`);
+
+    core.notice('Hello my custom JavaScript Action');
 }
 
 run();
