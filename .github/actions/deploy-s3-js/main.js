76d28ed35cc2e98c6c31747d6e8d3ea85d6eb37a
Md Adil Alam <mdadilalam@Mds-MacBook-Air.local>
2024-11-11 10:25:13 +0530

    add custome deploy action fix err 1

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -10,7 +10,7 @@ function run() {
 
     // 2 upload files
     const s3Uri = `s3://${bucket}`
-    exec.exec(`aws s3 sync  ${distFolder} ${s3Uri} -- region ${bucketRegion}`);
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 
     core.notice('Hello my custom JavaScript Action');
 }
